f404ee6c1664eca00e24bddf463bb74b952f15d7
Alejandro Chacón <alexchaconvargas@gmail.com>
2016-06-16 11:59:36 +0200

    updated index

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import {mySaga} from './sagas'
 import App from './components/App';
 import reducer from './reducer';
+import Home from './components/home/Home'
 import {VacancyManagerContainer} from './components/vacancyManager/VacancyManager';
 
+const sagaMiddleware = createSagaMiddleware();
+debugger;
 const store = createStore(reducer,
-	window.devToolsExtension ? window.devToolsExtension() : f => f);
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+  applyMiddleware(sagaMiddleware));
+
+
+
+sagaMiddleware.run(mySaga);
 store.dispatch({
   type: 'SET_STATE',
   state: {
@@ -20,8 +30,11 @@ store.dispatch({
 	}
 });
 
+
+
 const routes = <Route component={App}>
-  <Route path="/" component={VacancyManagerContainer} />
+  <Route path="/" component={Home} />
+  <Route path="/vacancyManager" component={VacancyManagerContainer} />
 </Route>;
 
 ReactDOM.render(
